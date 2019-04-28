5c4d3bb1e723c6676feb56441d5ff7b2d692ab91
Arseny Kalinichenko <49811679+ars-kalinichenko@users.noreply.github.com>
2019-04-28 08:42:50 +0600

    Update 4_19.cpp

--- 4_19.cpp
+++ 4_19.cpp
@@ -2,13 +2,12 @@
 #include <cstring>
 #include <windows.h>
 
-
 using namespace std;
 
-char* rev_str(char *s1) {
-    int l = strlen(s1);
-    char *res = new char [l];
-    for(int a = l-1, b = 0; a >= 0; a--, b++) {
+char *rev_str(char *s1) {
+    size_t l = strlen(s1);
+    char *res = new char[l];
+    for (int a = l - 1, b = 0; a >= 0; a--, b++) {
         res[b] = s1[a];
     }
     return res;
@@ -20,7 +19,7 @@ int main() {
 
     char s1_pr[100];
     cout << "Напишите любую строку: ";
-    cin.getline( s1_pr, 100 );
+    cin.getline(s1_pr, 100);
     cout << "Строка наоборот: " << rev_str(s1_pr);
 
     cin >> s1_pr;
