1725259ead7fd61a430d60884d5f84617b93ef29
Arseny Kalinichenko <49811679+ars-kalinichenko@users.noreply.github.com>
2019-04-28 08:49:48 +0600

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -4,21 +4,20 @@
 
 using namespace std;
 
-int main(void) {
-    double max, n;
+int main() {
+    double max = 0;
+    double n = 0;
     ifstream your_file("file.txt");
 
-    if (!your_file.is_open())
+    if (!your_file.is_open()) {
         cout << "Error opening file!";
-
-
-    else {
+    } else {
         vector<double> v;
         while (your_file >> n)
             v.push_back(n);
-        for (int i = 0; i < v.size(); i++)
-            if (v[i] > max)
-                max = v[i];
+        for (double i : v)
+            if (i > max)
+                max = i;
 
         your_file.close();
         cout << "Max: " << max;
